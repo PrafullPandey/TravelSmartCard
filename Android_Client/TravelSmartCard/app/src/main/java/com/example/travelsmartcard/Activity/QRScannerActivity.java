c9ce32b945b96e4625c54e07de7af3d4c0ccb407
Prafull Pandey <PrafullPandey@corp.fairisaac.com>
2019-11-16 17:47:32 +0530

    Qr Scenner Implemented

---
+++ Android_Client/TravelSmartCard/app/src/main/java/com/example/travelsmartcard/Activity/QRScannerActivity.java
@@ -0,0 +1,81 @@
+package com.example.travelsmartcard.Activity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.travelsmartcard.R;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Result;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class QRScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView zxingScannerView ;
+    public static final int MY_CAMERA_REQUEST_CODE = 10 ;
+    private static final String TAG = "QRScannerActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrscanner);
+
+        zxingScannerView = (ZXingScannerView)findViewById(R.id.qrCodeScanner);
+        setScannerProperties(zxingScannerView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA },
+                        MY_CAMERA_REQUEST_CODE);
+                return ;
+            }
+        }
+        zxingScannerView.startCamera();
+        zxingScannerView.setResultHandler(this);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    private void setScannerProperties(ZXingScannerView zxingScannerView) {
+        List list = new ArrayList<BarcodeFormat>();
+        list.add(BarcodeFormat.QR_CODE);
+        zxingScannerView.setFormats(list);
+        zxingScannerView.setAutoFocus(true);
+        zxingScannerView.setLaserColor(R.color.colorAccent);
+        zxingScannerView.setMaskColor(R.color.colorAccent);
+    }
+
+
+    @Override
+    public void handleResult(Result result) {
+        Log.v(TAG, result.getText()); // Prints scan results
+        Log.v(TAG, result.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
+
+        // If you would like to resume scanning, call this method below:
+        zxingScannerView.resumeCameraPreview(this);
+    }
+}
--- Android_Client/TravelSmartCard/app/src/main/java/com/example/travelsmartcard/Fragments/QR_Code_Scanner.java
+++ Android_Client/TravelSmartCard/app/src/main/java/com/example/travelsmartcard/Fragments/QR_Code_Scanner.java
@@ -1,19 +1,23 @@
 package com.example.travelsmartcard.Fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
+import com.example.travelsmartcard.Activity.QRScannerActivity;
 import com.example.travelsmartcard.R;
 
 public class QR_Code_Scanner extends Fragment {
 
 
     private OnFragmentInteractionListener mListener;
+    private ImageButton qrScanner ;
 
     public QR_Code_Scanner() {
         // Required empty public constructor
@@ -26,10 +30,16 @@ public class QR_Code_Scanner extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_qr__code__scanner, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_qr__code__scanner, container, false);
+        qrScanner = (ImageButton) view.findViewById(R.id.qrScanner);
+        qrScanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity() , QRScannerActivity.class));
+            }
+        });
+        return view;
     }
 
     public interface OnFragmentInteractionListener {
